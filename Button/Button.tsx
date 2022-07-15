7377c9771ff6034930b71f9d058218f04cf537d0
vitalii-mandzii <vitalii.mandziiwork@gmail.com>
2022-07-15 15:15:31 +0300

    Button

---
+++ Button/Button.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import s from './Button.module.scss';
+
+interface ButtonPropType {
+    variant: 'primary' | 'outlined' | 'link';
+    cb: any;
+    children?: React.ReactNode;
+    color?: 'secondary' | 'error' | 'success' | '';
+    disabled?: boolean;
+    size?: 'small'| 'medium' | 'large';
+}
+
+const Button = ({children, cb, variant = 'primary', color = '', disabled = false, size = 'medium'}: ButtonPropType) => {
+    const classNames = [
+        s.btn,
+        s[variant],
+        color ? s[color] : '',
+        size ? s[size] : '',
+        disabled ? s.disabled : '',
+    ];
+
+    const submitHandler = (e: any) => {
+        e.preventDefault()
+        cb()
+    }
+
+    return (
+        <button
+            onClick={submitHandler}
+            className={classNames.join(' ')}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
