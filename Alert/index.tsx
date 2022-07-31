ee26a23f9370f6e6bc57970d7b78af0b364b2483
Ivan Balias <balias.ivan.work@gmail.com>
2022-07-31 07:36:06 +0300

    fix avatar switch alert

--- Alert/index.tsx
+++ Alert/index.tsx
@@ -5,18 +5,20 @@ interface AlertProps {
     title?: string;
     description: string;
     type?: 'error' | 'success' | 'warning' | 'info';
-    variant?: 'filled' | 'outline'| 'default';
+    variant?: 'filled' | 'outline' | 'default';
     close?: boolean;
+    autoClose?: boolean;
 }
 
-const Alert = ({title = '', description = '', type = 'info', variant = 'default', close=false}: AlertProps) => {
+const Alert = ({title = '',description = '',type = 'info',variant = 'default',close = false,autoClose = true}: AlertProps) => {
 
     const [open, setOpen] = useState(true)
 
     useEffect(() => {
         const timeOut = setTimeout(() => {
-            // !close && setOpen(false)
-        }, 3000)
+            if (!close && autoClose)
+                setOpen(false)
+        }, 5000)
 
         return (() => {
             clearTimeout(timeOut)
@@ -33,17 +35,15 @@ const Alert = ({title = '', description = '', type = 'info', variant = 'default'
         s[type]
     ]
 
-    console.log(classes)
-
     return (
         open ?
             <div className={classes.join(' ')}>
-                <div className={s.img}></div>
+                <div className={s.img}> </div>
                 <div className={s.text}>
                     {title && <div className={s.title}>{title}</div>}
                     <div className={s.description}>{description}</div>
                 </div>
-                {close && <div className={s.close} onClick={closeHandler}>x</div>}
+                {close && <div className={s.close} onClick={closeHandler}>✖</div>}
             </div>
             :
             <></>
--- Avatar/index.tsx
+++ Avatar/index.tsx
@@ -4,13 +4,25 @@ import s from './styles.module.scss';
 interface AvatarProps {
     thumb?: string | null;
     name?: string;
+    surname?: string;
     cb?: any;
     size?: 'small' | 'medium' | 'large';
     type?: 'square' | 'circle' | 'rounded';
     bg?: string;
 }
 
-export const Avatar = ({thumb=null, bg='rgba(25, 118, 210, 1)',name='', cb=null, size='medium', type='circle'}: AvatarProps) => {
+export const Alert = ({thumb=null, bg='#2c3ea6',name='',surname='', cb=null, size='medium', type='circle'}: AvatarProps) => {
+
+    const fullName = () => {
+        const fullName:string[] = [];
+
+        if(name)
+            fullName.push(name.slice(0,1).toUpperCase())
+        if(surname)
+            fullName.push(surname.slice(0,1).toUpperCase())
+
+        return fullName.length ? fullName.join('') : null
+    }
 
     const classes = [
         s.avatar,
@@ -18,15 +30,10 @@ export const Avatar = ({thumb=null, bg='rgba(25, 118, 210, 1)',name='', cb=null,
         s[type]
     ];
 
-
-
     return (
         <div onClick={cb} style={{backgroundColor: bg}} className={classes.join(' ')}>
-            {thumb && <img src={thumb}/>}
-            {!thumb && name && <span>{name}</span>}
+            {thumb && <i style={{backgroundImage:`url(${thumb})`}}></i>}
+            {!thumb && fullName() && <span>{fullName()}</span>}
         </div>
     )
-
-
-
 }
